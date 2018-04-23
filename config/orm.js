e2d78c87b4d46c401ae25fb1d7dda0374b217fd4
JohnGit54 <johnp701@yahoo.com>
2018-04-23 18:29:02 -0400

    b1

--- config/orm.js
+++ config/orm.js
@@ -4,6 +4,47 @@ var connection = require("../config/connection"); // /config/connection.js");
 
 
 
+
+// copied from cats ::
+
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
 // required Methods for the ORM
 // selectAll()
 //insertOne()
@@ -15,16 +56,52 @@ var orm = {
     selectAll: function (tableInput, callback) {
         var querystring = " Select * from " + tableInput + ";";
 
-        connection.query(querystring,function(err,result){
-            if (err){
-                console.log (" Error on selectAll: ", err);
+        connection.query(querystring, function (err, result) {
+            if (err) {
+                console.log(" Error on selectAll: ", err);
                 throw err;
             }
             callback(result);
         })
+    },
+
+    insertOne: function (table, cols, vals, callback) {
+        var sql = "Insert into " + table;
+        sql += " ( " + cols.toString + " ) ";
+        sql += " Values ( " + printQuestionMarks(vals.length) + " ); ";
+
+        console.log(sql);
+
+        connection.query(sql, vals, function (err, result) {
+            if (err) {
+                console.log(" Error with Insert: ", err);
+                throw err;
+            }
+            //insert was succesful
+            callback(result)
+        })
+    },
+
+    // An example of objColVals would be {name: panther, sleepy: true}
+    updateOne: function (table, objColVals, condition, callback) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            callback(result);
+        });
     }
 
 
-
-
 }
+
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,31 @@
+//burger table model
+
+//import the ORM to create function that wil interact with teh database
+
+//var orm = require("../config/orm.js");
+import orm from "../config/orm.js";
+
+var burger = {
+
+    selectAll: function(callback){
+        orm.selectAll("'burgers", function(result){
+            callback(result);
+        })
+    },
+
+    insertOne: function(cols,vals,callback){
+        orm.insertOne("burgers", cols, vals, function(result){
+            callback(result);
+        }  )
+    },
+
+    updateOne: function( objColVals , condition , callback){
+        orm.updateOne("burgers",objColVals,condition,function(result){
+            callback(result);
+        })
+    }
+
+}
+
+//module.exports = burger;
+export default burger;
