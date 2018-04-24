538c0a9b9572e40f9ea19691bfafce70e266f028
JohnGit54 <johnp701@yahoo.com>
2018-04-24 13:09:55 -0400

    heroku JAWS info

--- config/connection.js
+++ config/connection.js
@@ -2,28 +2,43 @@
 //set up mySql connection to Burger_db
 
 var mysql = require('mysql');
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+    });
+};
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-})
-
-
-connection.connect(function (err) {
-    if (err) {
-        console.log("Error on connecting: ", err);
-        console.log("Error Stack, ", err.stack);
-        return;
-    }
-
-    //connection good here
-    console.log(" connected as id: " , connection.threadId);
-})
-
-
-//export the connection for our ORM to use
+connection.connect();
 module.exports = connection;
+
+
+// var connection = mysql.createConnection({
+//     port: 3306,
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+// })
+
+
+// connection.connect(function (err) {
+//     if (err) {
+//         console.log("Error on connecting: ", err);
+//         console.log("Error Stack, ", err.stack);
+//         return;
+//     }
+
+//     //connection good here
+//     console.log(" connected as id: " , connection.threadId);
+// })
+
+
+// //export the connection for our ORM to use
+//module.exports = connection;
